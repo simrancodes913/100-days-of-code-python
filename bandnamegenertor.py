1913a3dfbe1feff8415a723a9235101d8fb1df25
Simran <simran.official.in@gmail.com>
2025-06-09 19:24:40 +0530

    Add files via upload

---
+++ bandnamegenertor.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+a=input("What's the name of the city you grew up in?\n")
+print(a)
+b=input("What's your pet name?\n")
+print(b)
+print("Your Band name could be:"+ a+" "+b)
+
